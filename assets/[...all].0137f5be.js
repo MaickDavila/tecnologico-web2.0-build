c51ac88b00d41936b0fc89a3f13425b4e2fc9329
MaickDavila <librajunior13@gmail.com>
2021-10-25 21:59:02 -0500

    primer deploy

---
+++ assets/[...all].0137f5be.js
@@ -0,0 +1,1 @@
+import{e as o,K as t,o as s,b as a,t as r,j as c}from"./vendor.d59cf80d.js";var e={};const f=o({setup(p){const{t:n}=t();return(u,i)=>(s(),a("div",null,r(c(n)("not-found")),1))}});typeof e=="function"&&e(f);export{f as default};
